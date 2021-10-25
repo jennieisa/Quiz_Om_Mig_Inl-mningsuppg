0737e3282a63645927583c56d7674c4182c4bd09
jennieisa <jennieisakssoon97@gmail.com>
2021-10-25 17:23:09 +0200

    first commit

---
+++ krav.md
@@ -0,0 +1,25 @@
+Ankademin Quiz - JavaScript
+
+Beskrivning: 
+Du har fått i uppgift att skapa ett quiz med 10 frågor. Användare ska kunna svara på frågor och få feedback på huruvida hen besvarat dessa rätt eller fel.
+
+Krav:
+1. Quizet ska innehålla 10 frågor.
+
+2. Användaren ska utöver att besvara frågor för quizet, även kunna byta utseende på sidan till dark mode (dvs mörk bakgrundsfärg med ljus text ) och light mode (ljus bakgrundsfärg med mörk text).
+
+3. Användaren ska kunna göra om quizet med hjälp av ett knapptryck (OBS! Skapa t.ex en knapp som du kan åstadkomma detta med, användaren ska ej behöva uppdatera med funktionalitet som är inbyggd i webbläsaren).
+
+4. Uppgiften ska laddas upp på Github. Du behöver göra minst tre commits under arbetets gång.
+
+5. Under varje fråga ska det finnas minst tre svarsalternativ.
+
+6. Om svaret var korrekt eller inte ska detta ej skrivas ut direkt när användaren väljer ett svarsalternativ.
+
+7. Frågorna ska besvaras med <input type=”radio”> för svarsalternativen.
+
+8. En fråga kan besvaras med flera svar. Svarsalternativen (som ska vara minst tre stycken) ska då vara av typen <input type=”checkbox”>. För att få rätt på denna fråga, behöver samtliga rätta svarsalternativ vara ifyllda, varken fler eller mindre.
+
+9. Längst ned i quizet ska det finnas en knapp med texten “Check answers”. När den klickas på, ska det visas hur många rätt användaren hade (ska skrivas ut i DOM:en). Om användaren har över hälften rätt, ska meddelandet stå i orange färg. Om användaren har över 75% rätt, ska meddelandet stå i grön färg.
+
+Senast inlämnad 29/10
---
+++ main.js
@@ -0,0 +1,97 @@
+//Array med alla korrekta svar på radiobuttons frågor
+let correctAnswersRadioBtns = [
+     "24 år", "Södermalm", "Dalarna", "Min pojkvän", "Nej men jag vill SÅ gärna ha ett...", "Jobbade som reklamationshandläggare på Entercard", "Choklad", "Dokumentärer om mord", "Luktar på doftljus och scrollar Hemnet"
+]; 
+
+//Array med alla korrekta svar på checkbox frågan
+let correctAnswersCheckboxBtns = [
+     "Hamburgare från Barells", "Nästan allt som min pojkvän lagar", "Fish taco"
+]; 
+
+//Tom array där användarens valda radiobutton svar sparas 
+let usersAnswRadioBtn = [];
+//Tom array där användarens valda checkbox button svar sparas
+let usersAnswCheckboxBtn = [];
+//Hämtar rätta quizzet knappen
+let correctQuiz = document.querySelector("#submitBtn");
+//Variabel för att hålla koll på användarens poäng 
+let userPoints = 0;
+
+
+//Funktion som körs när man klickar på Rätta quizzet
+correctQuiz.addEventListener("click", () => {
+
+     let allRadioBtns = document.querySelectorAll(".radioBtnAnsw");
+     let allCheckboxBtns = document.querySelectorAll(".checkboxBtnsAnsw");
+
+     //Loopar igenom alla radiobuttons och kollar sedan vilka som är icheckade och sparar dess värde i arrayen usersAnswRadioBtn
+     allRadioBtns.forEach((btn) => {
+          if (btn.checked) {
+               usersAnswRadioBtn.push(btn.value); 
+          }
+     });
+
+     //Loopar igenom alla element i arrayen med användarens svar och om arrayen med dem rätta svaren innehåller elementen som finns i den så får man ett poäng per rätt svar
+     usersAnswRadioBtn.forEach((answ) => {
+          if (correctAnswersRadioBtns.includes(answ)){
+               userPoints++;
+          };
+     });
+
+     //Loopar igenom alla checkboxar och kollar sedan vilka som är icheckades och sparar dess värde i arrayen usersAnswCheckboxBtn
+     allCheckboxBtns.forEach((btn) => {
+          if(btn.checked) {
+               usersAnswCheckboxBtn.push(btn.value);
+          }
+     });
+
+     //Gör om arrayen med rätt svar för checkboxes och användarens valda checkboxes arrayen till string
+     let correctAnswersCheckboxBtnsToString = correctAnswersCheckboxBtns.toString();
+     let usersAnswCheckboxBtnToString = usersAnswCheckboxBtn.toString();
+
+     //Kollar om textsträngarna är lika och om de är det så plussa det på ett poäng
+     if(correctAnswersCheckboxBtnsToString == usersAnswCheckboxBtnToString){
+          userPoints++;
+     };
+
+     let quizResult = document.querySelector("#quizResult");
+
+     //Kollar hur många poäng användaren fått och utefter antalet skriver ut olika texter med olika färger. 
+     if (userPoints > 7.5 && userPoints >= 10){
+          quizResult.innerText = "Du fick " + userPoints + " av 10 rätt.";
+          quizResult.style.color = "green";
+     }else if (userPoints > 5 && userPoints < 7.5){
+          quizResult.innerText = "Du fick " + userPoints + " av 10 rätt.";
+          quizResult.style.color = "orange";
+     }else {
+          quizResult.innerText = "Du fick " + userPoints + " av 10 rätt";
+          quizResult.style.color = "black";
+     }
+});
+
+let retryQuizBtn = document.querySelector("#replyQuiz");
+
+//Funktion som körs när "starta om quizzet" klickas på.
+retryQuizBtn.addEventListener("click", () => {
+     location.reload();
+}); 
+
+let darkModeBtn = document.querySelector("#darkModeBtn");
+
+//Funktion som körs när man klickar på "mörkt läge" knappen
+darkModeBtn.addEventListener("click", () => {
+     document.querySelector("body").style.backgroundColor = "black";
+     document.querySelector("body").style.color = "magenta";
+     document.querySelector("#quizResult").style.backgroundColor = "white";
+}); 
+
+let lightModeBtn = document.querySelector("#lightModeBtn");
+
+//Funktion som körs när man klickar på "ljust läge" knappen
+lightModeBtn.addEventListener("click", () => {
+     document.querySelector("body").style.backgroundColor = "white";
+     document.querySelector("body").style.color ="black";
+}); 
+
+
+//! Göra snyggt och fint sen både i JS, CSS och HTML.
